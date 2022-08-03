b8331f22f59ad46122855460536dc725e8fa440f
Ricardo Ruíz <rich.ruiz.v@gmail.com>
2022-08-03 12:32:12 -0500

    creation of home page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "pages#home"
   resources :articles
   get 'signup', to:'users#new'
   post 'users', to:'users#create'
