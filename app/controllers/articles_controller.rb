ca70a7ed634b17e460ec723d858a33e3899186b0
Ricardo Ruíz <rich.ruiz.v@gmail.com>
2022-08-13 21:27:43 -0500

    redirections between articles and users

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -28,7 +28,7 @@ class ArticlesController < ApplicationController
 
   def update
     if @article.update(article_params)
-      flash[:notice] = "Article was update successfully!"
+      flash[:notice] = "Article was updated successfully!"
       redirect_to @article
     else
       render 'edit'
