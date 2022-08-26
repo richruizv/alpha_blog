9db8aca67cba48f9ea025570bd058916d4f63497
Ricardo Ruíz <rich.ruiz.v@gmail.com>
2022-08-26 17:32:58 -0500

    destroy own account fix create user without require login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show,:edit,:update]
-  before_action :require_user, except: [:index, :show]
+  before_action :set_user, only: [:show,:edit,:update,:destroy]
+  before_action :require_user, except: [:index, :show, :new, :create]
   before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def show
@@ -36,6 +36,13 @@ class UsersController < ApplicationController
     else
         render 'edit'
     end
+  end
+
+  def destroy
+    @user.destroy
+    session[:user_id] = nil
+    flash[:notice] = "Account and all associated articles succesfully deleted"
+    redirect_to articles_path
   end
 
   private
